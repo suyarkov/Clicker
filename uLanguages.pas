7fbd8c6d3e641aef0032f2b13b3c5c11dd4a0081
Dmitry S <suyarkov@gmail.com>
2023-02-25 09:53:03 +0300

    Языки правим

--- uLanguages.pas
+++ uLanguages.pas
@@ -7,15 +7,30 @@ uses
   System.NetEncoding;
 
   type TLanguage = record
-      Id  	    : integer; // номер по порядку
-      LnCode	  : string;
-      EnterName	: string;
-      ReadName  : string;
+      Id  	        : integer; // номер по порядку
+      LnCode	      : string;
+      NameForEnter	: string;
+      NameForRead   : string;
   end;
   type TListLanguages = Array [1..1000] of TLanguage;
 
-  function Languages(const AValue, ConstSourceLang, ConstTargetLang: String): String;
+  function InitListLanguages(): TListLanguages;
 
 implementation
+
+  function InitListLanguages(): TListLanguages;
+  var
+    vList : TListLanguages;
+    i   : integer;
+  begin
+    i := 1;
+    vList[i].Id := i;
+    vList[i].LnCode := 'ru';
+    vList[i].NameForEnter := 'ru';
+    vList[i].NameForRead := 'ru';
+
+
+    result := vList;
+  end;
 
 end.
